9d5ea88d36fa3b1f7bcb38f67dd1ad6deeafa768
unknown <sharmakeshav54126@gmail.com>
2024-07-29 04:07:43 +0530

    husky ....

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 cons a=10;
+const a{}
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
